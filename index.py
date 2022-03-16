69f5657951a94dfc6d6f3f16721cc753643396b5
devcastro9 <castro.q.orlando@gmail.com>
2022-03-16 14:07:32 -0400

    read operation mdb in py

--- index.py
+++ index.py
@@ -1,2 +1,22 @@
+"""
+    Conexion a la base de datos MS Access
+"""
+import adodbapi as db
+
+def main():
+    constr = 'Provider=Microsoft.ACE.OLEDB.12.0;Data Source=PARAMETROS.mdb'
+    sql = "select * from gc_parametros_sistema"
+    header = '| '
+    conn = db.connect(constr)
+    with conn.cursor() as c:
+        c.execute(sql)
+        for d in c.description:
+            header += d[0] + ' | '
+        print(header)
+        result = c.fetchmany(1)
+        for i in result:
+            print(i)
+    conn.close
+
 if __name__ == "__main__":
-    pass
+    main()
